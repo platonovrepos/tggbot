2cc08944a9af73ed020cc801500e1dd5a1b7302a
platonovrepos <pythonlearn91@gmail.com>
2023-05-17 23:48:56 +0500

    df

--- file2.py
+++ file2.py
@@ -1,5 +1,10 @@
 import os
-from dotenv import load_dotenv
+from dotenv_vault import load_dotenv
 
-load_dotenv(DOMAIN)
-print(DOMAIN)
+load_dotenv()
+s3_fdf = os.getenv("hello")
+print(s3_fdf)
+
+# Code of your application, which uses environment variables (e.g. from `os.environ` or
+# `os.getenv`) as if they came from the actual environment.
+
